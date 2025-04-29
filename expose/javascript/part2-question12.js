4a12c43b2ed8c274d4160a825cedd5194a590323
victoria tran <108353792+victoriatr6n@users.noreply.github.com>
2025-04-28 21:34:44 -0700

    Update part2-question12.js

--- expose/javascript/part2-question12.js
+++ expose/javascript/part2-question12.js
@@ -9,5 +9,13 @@ let student = {
     },
   courseLoad: ['CSE 110', 'CSE 134', 'VIS 41']
 };
-
-let name = student.name
+//part A:
+let student_name = student.name
+//part B:
+let grad_year = student["Grad Year"]
+//part C:
+let greeting = student.greeting()
+//part D:
+let teacher = student["Favorite Teacher"].name
+//part E:
+let first_course = student.courseLoad[0]
