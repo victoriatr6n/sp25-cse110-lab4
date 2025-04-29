0a1fc4cd6d47ff54043652c82b5f212c637bb579
victoria tran <108353792+victoriatr6n@users.noreply.github.com>
2025-04-28 23:00:14 -0700

    Update part2-question12.js

--- expose/javascript/part2-question12.js
+++ expose/javascript/part2-question12.js
@@ -1,21 +1,14 @@
-let student = {
-  name: 'Sarah',
-  major: 'Computer Science',
-  'Grad Year': '2022',
-  greeting: function() { console.log("Hello!");},
-  'Favorite Teacher': {
-    name: 'Thomas Powell',
-    course: 'CSE 110'
-    },
-  courseLoad: ['CSE 110', 'CSE 134', 'VIS 41']
+let statistics = {
+  redCars: 21,
+  blueCars: 45,
+  greenCars: 12,
+  raceCars: 5,
+  blackCars: 40,
+  rareCars: 2,
 };
-//part A:
-let student_name = student.name
-//part B:
-let grad_year = student["Grad Year"]
-//part C:
-let greeting = student.greeting()
-//part D:
-let teacher = student["Favorite Teacher"].name
-//part E:
-let first_course = student.courseLoad[0]
+
+for(const car in statistics){
+  if ((car[0] == r) || (statistics[car] % 2 == 1)){
+    console.log(statistics[car])
+  }
+}
